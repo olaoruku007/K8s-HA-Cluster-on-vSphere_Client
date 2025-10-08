e8bc2965cd879c77cef9c2207843490885de22e5
olaoruku007 <93499206+olaoruku007@users.noreply.github.com>
2025-10-08 00:01:24 -0400

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,26 @@
+output "master_ips" {
+  description = "Master node IP addresses"
+  value = [
+    for vm in vsphere_virtual_machine.k8s_master : 
+    vm.default_ip_address
+  ]
+}
+
+output "worker_ips" {
+  description = "Worker node IP addresses"
+  value = [
+    for vm in vsphere_virtual_machine.k8s_worker : 
+    vm.default_ip_address
+  ]
+}
+
+output "master_names" {
+  description = "Master node names"
+  value       = vsphere_virtual_machine.k8s_master[*].name
+}
+
+output "worker_names" {
+  description = "Worker node names"
+  value       = vsphere_virtual_machine.k8s_worker[*].name
+}
 
